8cd1ac8927d2d878023b70d73b854059995ab6a9
junsoo0 <1herjs1@gmail.com>
2022-12-01 01:42:51 +0900

    add patty dialog

--- app/src/main/java/com/example/kiosk/MenuManagementActivity.kt
+++ app/src/main/java/com/example/kiosk/MenuManagementActivity.kt
@@ -2,12 +2,27 @@ package com.example.kiosk
 
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kiosk.databinding.MenuManagementBinding
+import com.example.kiosk.databinding.PattyBinding
 
 class MenuManagementActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.menu_management)
+
+        val binding = MenuManagementBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pattyButton.setOnClickListener {
+            val dialogBinding = PattyBinding.inflate(layoutInflater)
+            AlertDialog.Builder(this).run {
+                setTitle("패티")
+                setView(dialogBinding.root)
+                setPositiveButton("닫기", null)
+                show()
+            }
+        }
 
         val btnReturn = findViewById<Button>(R.id.menu_management_back_button)
         btnReturn.setOnClickListener {
