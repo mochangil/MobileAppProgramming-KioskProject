33067b88556b3cae043d2cb78aac7f3707b71de7
junsoo0 <1herjs1@gmail.com>
2022-12-01 00:33:38 +0900

    add MainActivity

--- app/src/main/java/com/example/kiosk/MainActivity.kt
+++ app/src/main/java/com/example/kiosk/MainActivity.kt
@@ -1,19 +1,11 @@
 package com.example.kiosk
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.admin)
-
-        val btnNewActivity = findViewById<Button>(R.id.admin_button2)
-        btnNewActivity.setOnClickListener {
-            val intent = Intent(this, MenuManagementActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_main)
     }
 }
