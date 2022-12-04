0de3bc0eb493d0b643ac8ce1ef4b6fbbe09da034
junsoo0 <1herjs1@gmail.com>
2022-12-05 00:07:36 +0900

    add cash payment

--- app/src/main/java/com/example/kiosk/CardPaymentFragment.kt
+++ app/src/main/java/com/example/kiosk/CardPaymentFragment.kt
@@ -1,13 +1,14 @@
 package com.example.kiosk
 
 import android.os.Bundle
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.RadioGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +17,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PrePaymentFragment.newInstance] factory method to
+ * Use the [CardPaymentFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PrePaymentFragment : Fragment() {
+class CardPaymentFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -30,6 +31,8 @@ class PrePaymentFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
@@ -37,33 +40,34 @@ class PrePaymentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val v = inflater.inflate(R.layout.fragment_pre_payment, container, false)
-        v.findViewById<Button>(R.id.pre_payment_cancel).setOnClickListener {
+
+        val v = inflater.inflate(R.layout.fragment_card_payment, container, false)
+
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setView(R.layout.payment_dialog)
+        builder.setCancelable(false)
+        val dialog = builder.create()
+
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            if(activity is PaymentActivity) {
+                dialog.show()
+            }
+        }, 5000)
+
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            if(activity is PaymentActivity) {
+                dialog.dismiss()
+            }
+        }, 10000)
+
+        dialog.setOnDismissListener() {
+            activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.fragment_container, AfterPaymentFragment())?.commit()
+        }
+
+        v.findViewById<Button>(R.id.payment_cancel).setOnClickListener {
             Toast.makeText(activity, "결제 취소하셨습니다", Toast.LENGTH_SHORT).show()
             activity?.finish()
         }
-        val confirmButton = v.findViewById<Button>(R.id.pre_payment_confirm)
-        val rg1 = v.findViewById<RadioGroup>(R.id.meal_location_radio)
-        val rg2 = v.findViewById<RadioGroup>(R.id.payment_method_radio)
-
-        rg1.setOnCheckedChangeListener { _, _ ->
-            if(rg2.checkedRadioButtonId != -1) {
-                confirmButton.isEnabled = true
-            }
-        }
-        rg2.setOnCheckedChangeListener { _, _ ->
-            if(rg1.checkedRadioButtonId != -1) {
-                confirmButton.isEnabled = true
-            }
-        }
-
-        confirmButton.setOnClickListener {
-            if(rg2.checkedRadioButtonId == R.id.cash)
-                activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.fragment_container, CashPaymentFragment())?.commit()
-            else
-                activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.fragment_container, CardPaymentFragment())?.commit()
-        }
-
         return v
     }
 
@@ -74,12 +78,12 @@ class PrePaymentFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PrePaymentFragment.
+         * @return A new instance of fragment before_payment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PrePaymentFragment().apply {
+            CardPaymentFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/kiosk/CashPaymentFragment.kt
+++ app/src/main/java/com/example/kiosk/CashPaymentFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +16,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BeforePaymentFragment.newInstance] factory method to
+ * Use the [CashPaymentFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BeforePaymentFragment : Fragment() {
+class CashPaymentFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -31,8 +30,6 @@ class BeforePaymentFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
-
     }
 
     override fun onCreateView(
@@ -40,29 +37,11 @@ class BeforePaymentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        val v = inflater.inflate(R.layout.fragment_before_payment, container, false)
-
-        val builder = AlertDialog.Builder(requireActivity())
-        builder.setView(R.layout.payment_dialog)
-        builder.setCancelable(false)
-        val dialog = builder.create()
+        val v = inflater.inflate(R.layout.fragment_cash_payment, container, false)
 
         android.os.Handler(Looper.getMainLooper()).postDelayed({
-            if(activity is PaymentActivity) {
-                dialog.show()
-            }
-        }, 5000)
-
-        android.os.Handler(Looper.getMainLooper()).postDelayed({
-            if(activity is PaymentActivity) {
-                dialog.dismiss()
-            }
-        }, 10000)
-
-        dialog.setOnDismissListener() {
             activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.fragment_container, AfterPaymentFragment())?.commit()
-        }
+        }, 10000)
 
         v.findViewById<Button>(R.id.payment_cancel).setOnClickListener {
             Toast.makeText(activity, "결제 취소하셨습니다", Toast.LENGTH_SHORT).show()
@@ -78,12 +57,12 @@ class BeforePaymentFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment before_payment.
+         * @return A new instance of fragment CashPaymentFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BeforePaymentFragment().apply {
+            CashPaymentFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
